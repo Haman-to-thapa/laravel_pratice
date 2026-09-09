de13277784e64354dd9d50061ce52f23f0832506
Heman <hemanta@triplewsols.com>
2026-09-09 12:31:43 +0530

    MySQL DB Connection with Laravel

--- user-management/app/Http/Controllers/UserController.php
+++ user-management/app/Http/Controllers/UserController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class UserController extends Controller
 {
     //
+    function users() {
+        // return"User function";
+        return DB::select('select * from users');
+    }
 }
