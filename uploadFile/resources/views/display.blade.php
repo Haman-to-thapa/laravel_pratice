7e87816831cfbeef6b94d1e3974449001701b2c9
Heman <hemanta@triplewsols.com>
2026-09-19 15:53:24 +0530

    image upload

---
+++ uploadFile/resources/views/display.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
+<div>
+    <img src="{{ asset('storage/' . $path) }}" alt="Uploaded Image">
+</div>
+</div>
